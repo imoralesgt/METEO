6ef836a18d89055d685156780d1456d86be78a0f
imoralesgt <ivan.rene.morales@gmail.com>
2018-06-19 23:45:07 -0600

    Added light sensor-related defines

--- piSensors/sensorDefs.py
+++ piSensors/sensorDefs.py
@@ -1,3 +1,9 @@
+
+'''
+=======================================================
+IRM BME680 Defines
+=======================================================
+'''
 GAS_HEATER_TEMPERATURE = 320 #IRM BME680's gas heater operating temperature
 GAS_HEATER_DURATION    = 150 #IRM BME680's gas heater startup duration (seconds)
 GAS_HEATER_PROFILE     = 0   #IRM BME680's chosen gas heating profile
@@ -7,3 +13,13 @@ GAS_BURN_IN_TIME = 300 #IRM Burn-in time (seconds) before gas sensor is operativ
 GAS_BASELINE_MEASUREMENTS = 50 #IRM How many gas sensor measurements should be used for average GAS_BASELINE_MEASUREMENT
 
 HUM_BASELINE = 40 #IRM Optimal indoor relative humidity percentage (default: 40%)
+
+
+
+'''
+=======================================================
+IRM BH1750 Defines
+=======================================================
+'''
+DEFAULT_LIGHT_SENSITIVITY = 69
+DEFAULT_LIGHT_I2C_ADDRESS = 0x23
